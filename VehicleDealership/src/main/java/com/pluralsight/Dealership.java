a6dcee2cf95cac070d798c04473db514c441ecf3
corcor322 <corcor322@gmail.com>
2024-10-25 13:49:03 -0400

    Built addVehicle method, completing Phase 1

--- VehicleDealership/src/main/java/com/pluralsight/Dealership.java
+++ VehicleDealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,8 +1,10 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Dealership {
+    Scanner scanner = new Scanner(System.in);
     String name;
     String address;
     String phone;
@@ -13,6 +15,8 @@ public class Dealership {
         this.name = name;
         this.address = address;
         this.phone = phone;
+
+        // TODO: Instantiate ArrayList<Vehicle> object in constructor
     }
     public ArrayList<Vehicle> getVehiclesByPrice() {
         return null;
@@ -35,7 +39,35 @@ public class Dealership {
     public ArrayList<Vehicle> getAllVehicles() {
         return null;
     }
-    public void addVehicle() {
+    public void addVehicle(Scanner scanner) {
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        System.out.println("Enter the VIN of the vehicle you would like to add.");
+        int vin = scanner.nextInt();
+
+        System.out.println("Enter the year of the vehicle you would like to add.");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the vehicle make.");
+        String make = scanner.nextLine();
+
+        System.out.println("Enter the vehicle model.");
+        String model = scanner.nextLine();
+
+        System.out.println("Enter the color of the vehicle.");
+        String color = scanner.nextLine();
+
+        System.out.println("Enter the mileage of the vehicle.");
+        int odometer = scanner.nextInt();
+
+        System.out.println("Enter the price of the vehicle.");
+        double price = scanner.nextDouble();
+
+        System.out.println("Thank you! Vehicle has been added.");
+
+        Vehicle vehicle = new Vehicle(vin, year, make, model, color, odometer, price);
+        vehicles.add(vehicle); //Add vehicle to array and to file
+
 
     }
     public void removeVehicle() {
