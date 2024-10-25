a60496ee463bd164af777afc85a778eab513b7a8
corcor322 <corcor322@gmail.com>
2024-10-25 11:59:42 -0400

    Built methods for Dealership class with temporary null return values

--- VehicleDealership/src/main/java/com/pluralsight/Dealership.java
+++ VehicleDealership/src/main/java/com/pluralsight/Dealership.java
@@ -8,4 +8,37 @@ public class Dealership {
     String phone;
 
     ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    public ArrayList<Vehicle> getVehiclesByPrice() {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByMakeModel() {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByYear() {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByColor() {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByMileage() {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByType() {
+        return null;
+    }
+    public ArrayList<Vehicle> getAllVehicles() {
+        return null;
+    }
+    public void addVehicle() {
+
+    }
+    public void removeVehicle() {
+
+    }
 }
