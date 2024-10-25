976084b219d7ea7c4b3e480783f376a8d3fb002f
corcor322 <corcor322@gmail.com>
2024-10-25 11:40:36 -0400

    Added dealership attributes and Vehicle inventory array list to Dealership class

--- VehicleDealership/src/main/java/com/pluralsight/Dealership.java
+++ VehicleDealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    String name;
+    String address;
+    String phone;
+
+    ArrayList<Vehicle> inventory = new ArrayList<>();
 }
