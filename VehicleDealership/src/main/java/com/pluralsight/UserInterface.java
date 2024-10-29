f047840f2cae5843fecff4def9d85953d3b21e91
corcor322 <corcor322@gmail.com>
2024-10-28 20:56:59 -0400

    Built UserInterface skeleton according to phase 3 class diagram.

--- VehicleDealership/src/main/java/com/pluralsight/UserInterface.java
+++ VehicleDealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
 public class UserInterface {
+    private Dealership dealership;
+
+    public void processGetByPriceRequest() {
+
+    }
+    public void processGetByMakeModelRequest() {
+
+    }
+    public void processGetByYearRequest() {
+
+    }
+    public void processGetByColorRequest() {
+
+    }
+    public void processGetByMileageRequest() {
+
+    }
+    public void processGetByVehicleTypeRequest() {
+
+    }
+    public void processGetAllVehiclesRequest() {
+
+    }
+    public void processAddVehicleRequest() {
+
+    }
+    public void processRemoveVehicleRequest () {
+
+    }
 }
